4dc38a850274e574f5779903e51ce982bbbf22db
Danijel Bogicevic <dan.bogicevic@gmail.com>
2024-12-04 12:53:55 +0100

    -fix

---
+++ CarRental/CarRentalContext.cs
@@ -0,0 +1,16 @@
+﻿using CarRental.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarRental
+{
+    public class CarRentalContext : DbContext
+    {
+        public CarRentalContext(DbContextOptions<CarRentalContext> options) : base(options) { }
+
+        public DbSet<Car> Cars {  get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+
+        //public DbSet<Review> Reviews {  get; set; }
+        //public DbSet<User> Users{  get; set; }
+    }
+}
---
+++ CarRental/Models/ViewModels/Car/IndexViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace CarRental.Models.ViewModels.Car
+{
+    public class IndexViewModel
+    {
+        public IEnumerable<Models.Car> Cars { get; set; }
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public decimal? MinPrice { get; set; }
+        public decimal? MaxPrice { get; set; }
+    }
+}
